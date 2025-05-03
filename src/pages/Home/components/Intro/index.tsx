d8985626389a6fb65f8e967599a6e7483c5c6af6
Paulo <pvvtoledo@gmail.com>
2025-05-03 10:16:40 -0300

    Intro finalizada

--- src/pages/Home/components/Intro/index.tsx
+++ src/pages/Home/components/Intro/index.tsx
@@ -1,13 +1,73 @@
-import {  IntroContainer } from "./styles";
+
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
 import background from '../../../../assets/background.svg'
 import coffeeTitle from '../../../../assets/coffee-title.png'
-import { Title } from "./Title";
+import { Heading, Hero, HeroContent, Info } from './styles'
+import { useTheme } from 'styled-components'
 
-export function Intro () {
+
+export function Intro() {
+    const theme = useTheme()
     return (
-        <IntroContainer bgImage={background}>
-            <Title />
-            <img src={coffeeTitle} alt="" />
-        </IntroContainer>
+        <Hero>
+            <HeroContent>
+                <div>
+                    <Heading>
+                        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+
+                        <span>
+                            Com o Coffee Delivery você recebe seu café onde estiver, a
+                            qualquer hora
+                        </span>
+                    </Heading>
+
+                    <Info>
+                        <div>
+                            <ShoppingCart
+                                size={32}
+                                weight="fill"
+                                color={theme.colors.background}
+                                style={{ backgroundColor: theme.colors['yellow-dark'] }}
+                            />
+                            <span>Compra simples e segura</span>
+                        </div>
+                        <div>
+                            <Package
+                                size={32}
+                                weight="fill"
+                                color={theme.colors.background}
+                                style={{ backgroundColor: theme.colors['base-text'] }}
+                            />
+                            <span>Embalagem mantém o café intacto</span>
+                        </div>
+
+                        <div>
+                            <Timer
+                                size={32}
+                                weight="fill"
+                                color={theme.colors.background}
+                                style={{ backgroundColor: theme.colors.yellow }}
+                            />
+                            <span>Entrega rápida e rastreada</span>
+                        </div>
+
+                        <div>
+                            <Coffee
+                                size={32}
+                                weight="fill"
+                                color={theme.colors.background}
+                                style={{ backgroundColor: theme.colors.purple }}
+                            />
+                            <span>O café chega fresquinho até você</span>
+                        </div>
+
+                    </Info>
+                    
+                </div>
+
+                <img src={coffeeTitle} />
+            </HeroContent>
+            <img src={background} id="hero-bg" />
+        </Hero>
     )
 }
--- src/pages/Home/components/Intro/styles.ts
+++ src/pages/Home/components/Intro/styles.ts
@@ -1,20 +1,76 @@
 import styled from "styled-components";
 
-interface IntroContainerProps {
-  bgImage: string
-}
-
-
-export const IntroContainer = styled.div<IntroContainerProps>`
+export const Hero = styled.div`
   position: relative;
+
+  img#hero-bg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    max-height: 544px;
+    width: 100vw;
+    object-fit: cover;
+  }
+`
+
+export const HeroContent = styled.div`
+  max-width: 1160px;
+  padding: 5.75rem 1.25rem;
+  margin: 0 auto;
+
   display: flex;
-  align-items: center;
-  justify-content: center;
   gap: 3.5rem;
-  background-image: url(${props => props.bgImage});
-  background-size: cover;
-  background-position: center;
-  width: 100%;
-  height: auto;
-   
+  align-items: flex-start;
+  justify-content: space-between;
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    gap: 4.125rem;
+  }
+`
+
+export const Heading = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    > h1 {
+      font-family: ${props => props.theme.typography.baloo2["font-family"]};
+      font-size: ${props => props.theme.typography.baloo2.TitleXL["font-size"]};
+      font-weight: ${props => props.theme.typography.baloo2.TitleXL["font-weight"]};
+      line-height: ${props => props.theme.typography.baloo2.TitleXL["line-height"]};
+    }
+
+    > span {
+      font-family: ${props => props.theme.typography.roboto["font-family"]};
+      font-size: ${props => props.theme.typography.roboto.TitleL["font-size"]};
+      font-weight: ${props => props.theme.typography.roboto.TitleL.fontWeight.regular};
+      line-height: ${props => props.theme.typography.roboto.TitleL["line-height"]};
+    }
+
+`
+
+export const Info = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-row-gap: 1.25rem;
+
+  > div {
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+
+    svg {
+      padding: 0.5rem;
+      border-radius: 999px;
+    }
+
+    span {
+      font-family: ${props => props.theme.typography.roboto["font-family"]};
+      font-size: ${props => props.theme.typography.roboto.TitleM["font-size"]};
+      font-weight: ${props => props.theme.typography.roboto.TitleM.fontWeight.regular};
+      line-height: ${props => props.theme.typography.roboto.TitleM["line-height"]};
+    }
+  }
 `
